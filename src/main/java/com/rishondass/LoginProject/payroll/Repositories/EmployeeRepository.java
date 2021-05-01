b385c5a55b640a90c15b16c9c7c97f2f6d96682b
Rishon Dass <73409026+rdass1@users.noreply.github.com>
2021-05-01 03:20:45 -0500

    Delete src directory

--- src/main/java/com/rishondass/LoginProject/payroll/Repositories/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rishondass.loginSystem.payroll.Repositories;
-
-import com.rishondass.loginSystem.payroll.Models.EmployeeModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeModel, Integer> {
-}
